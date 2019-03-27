b81d9496722e6318009075228c1b8a7b8c5e5637
getimiskon <31846052+getimiskon@users.noreply.github.com>
2019-03-27 12:49:24 +0200

    Script file added
    It still needs a few fixes

---
+++ linein.sh
@@ -0,0 +1,30 @@
+#/bin/zsh
+echo LINE IN ENABLER "(PULSEAUDIO)" v1.0
+echo
+echo IN ORDER TO LISTEN TO LINE IN INPUT GO TO CONFIGURATION TAB
+echo AND CHANGE THE PROFILE TO ANALOG STEREO DUPLEX
+echo 
+echo TYPE E TO ENABLE LINE IN INPUT
+echo OR TYPE D TO DISABLE IT.
+echo "(USE ONLY UPPERCASE CHARACTERS)"
+echo
+read LINE_IN
+echo
+if [ $LINE_IN = E ]
+then
+	echo SET LATENCY "(MSEC)":
+	read LATENCY
+	pacmd load-module module-loopback latency_msec=$LATENCY
+	echo
+	echo LINE IN INPUT ENABLED!
+else
+	if [ $LINE_IN = D ]
+	then
+		pacmd unload-module module-loopback
+		echo LINE IN INPUT DISABLED!
+	else
+		echo ERROR!
+	fi
+fi
+	
+#THE SCRIPT IS CREATED BY GETIMISKON
